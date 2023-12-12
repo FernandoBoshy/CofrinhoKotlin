125b771241205a9bbae2412e0b79b5a2c3883f9b
Fernando Augusto De Oliveira <fernando.augusto@TRC.local>
2023-12-12 19:40:05 -0300

    primeiro commit

---
+++ src/main/kotlin/Classes/Moedas.kt
@@ -0,0 +1,30 @@
+package Classes
+
+open class Moedas {
+    val sigla: String = ""
+    val cotacao: String = ""
+    var quant: Double = 0.0
+
+    fun atualizarDinheiro(valor: Double){
+        if (valor <= 0.0){
+            println("Não aceita valores negativos, nem valor Zero.")
+        }
+        else {
+            quant += valor
+        }
+    }
+    fun retirarDinheiro(x: Double){
+        var valor = x
+        if (valor == 0.0){
+            println("Não aceita valor Zero")
+        }
+        else {
+            if(valor < 0.0){
+                valor *= -1.0
+            }
+            quant -= valor
+        }
+
+        }
+    }
+}
